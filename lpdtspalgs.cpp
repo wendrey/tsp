961a3f6a501b0e28a5d808ede3c8cdd1adfab675
wendrey <lordwendrey@gmail.com>
2016-12-01 21:29:21 -0200

    Projeto 03

--- lpdtspalgs.cpp
+++ lpdtspalgs.cpp
@@ -9,6 +9,9 @@
  * RA: 148234			   *
  ***************************/
 
+#include "SampleDecoder.h"
+#include "MTRand.h"
+#include "BRKGA.h"
 #include <iostream>
 #include <float.h>
 #include <lemon/list_graph.h>
@@ -141,12 +144,126 @@ bool constrHeur(const LpdTspInstance &l, LpdTspSolution  &s, int tl) {
 }
 
 //------------------------------------------------------------------------------
+
+// Heurística do BRKGA para obter solução
+
+std::vector<double> brkga(const LpdTspInstance &l, int tl) {
+
+	clock_t st = clock();		// Tempo máximo para rodar o algoritmo
+
+	const unsigned n = l.n-1;	// size of chromosomes
+	const unsigned p = 100;		// size of population
+	const double pe = 0.20;		// fraction of population to be the elite-set
+	const double pm = 0.10;		// fraction of population to be replaced by mutants
+	const double rhoe = 0.70;	// probability that offspring inherit an allele from elite parent
+	const unsigned K = 3;		// number of independent populations
+	const unsigned MAXT = 2;	// number of threads for parallel decoding
+	
+	SampleDecoder decoder;			// initialize the decoder
+	
+	const long unsigned rngSeed = 0;	// seed to the random number generator
+	MTRand rng(rngSeed);				// initialize the random number generator
+	
+	// initialize the BRKGA-based heuristic
+	BRKGA< SampleDecoder, MTRand > algorithm(n, p, pe, pm, rhoe, decoder, rng, K, MAXT);
+	
+	unsigned generation = 0;		// current generation
+	const unsigned X_INTVL = 20;	// exchange best individuals at every 20 generations
+	const unsigned X_NUMBER = 3;	// exchange top 3 best
+	const unsigned MAX_GENS = 100;	// run for 100 gens
+	
+	do {
+		algorithm.evolve();	// evolve the population for one generation
+		
+		if((++generation) % X_INTVL == 0) {
+			algorithm.exchangeElite(X_NUMBER);	// exchange top individuals
+		}
+	} while (generation < MAX_GENS || (tl < clock() - st / CLOCKS_PER_SEC));
+
+	// Retorna o melhor resultado obtido
+	return algorithm.getBestChromosome;
+
+}
+
+//------------------------------------------------------------------------------
+
+// Meta-heurística para obter solução
+
 bool metaHeur(const LpdTspInstance &l, LpdTspSolution  &s, int tl) {
 
+	double cost = 0.0;
+
+	// Recupera o melhor resultado obtido pelo brkga
+
+	std::vector<double> chromosome = brkga(l, tl);
+
+	std::vector <std::pair<double,DNode>> ranking(chromosome.size());
+
+	for (DNodeIt n(l.g), int k = 0; n != INVALID; ++n, k++)
+		if (n != l.depot)
+			ranking[k] = std::pair <double,DNode> (chromosome[k],n);
 	
+	vector <DNode> tour;
+	tour.push_back(l.depot);
 
+	for(std::vector<std::pair<double,DNode>>::const_iterator i = ranking.begin(); i != ranking.end(); ++i)	
+		tour.push_back(i->second);
+	
+	// Verifica se existe aresta ligando os vertices
 
-   return false;
+	for(int i = 0; i < (int) tour.size(); i++) {
+		OutArcIt o(l.g, tour[i]);
+		for(; o != INVALID; ++o)
+			if(l.g.target(o) == tour[(i+1) % (int)tour.size()])
+				break;
+		if(o == INVALID)
+			return false;
+		else
+			cost += l.weight[o];
+	}
+	
+	// Verifica a ordem de coleta e entrega dos itens
+	
+	vector<bool> ss(l.k, false);
+	vector<bool> tt(l.k, false);
+
+	for(int i = 1; i < (int)tour.size(); i++) {
+		if(l.s[tour[i]] > 0) {
+			if(!ss[l.s[tour[i]]] && !tt[l.s[tour[i]]])
+				ss[l.s[tour[i]]] = true;
+			else
+				return false;
+		}
+		else if(l.t[tour[i]] > 0) {
+			if(ss[l.t[tour[i]]] && !tt[l.t[tour[i]]])
+				tt[l.t[tour[i]]] = true;
+			else
+				return false;
+		}
+		else {
+				return false;
+		}
+	}
+
+	// Verifica se a quantidade carregada é menor que a capacidade permitida
+
+	double load = 0.0;
+
+	for(int i = 0; i < (int)tour.size(); i++) {
+		if(l.t[tour[i]] > 0 )
+			load -= l.items[l.t[tour[i]]].w;
+		if(l.s[tour[i]] > 0 )
+			load += l.items[l.s[tour[i]]].w;
+		if(load < (-1)*MY_EPS)
+			return false;
+		if(load > instance.capacity)
+			return false;
+	}
+
+	s.cost = cost;
+	s.tour = tour;
+
+	return false;
 
 }
 //------------------------------------------------------------------------------
